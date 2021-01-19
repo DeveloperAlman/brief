1b31c4558b3a3323f6fa45696e994cfe75cf4e48
DeveloperAlman <67116730+DeveloperAlman@users.noreply.github.com>
2021-01-19 14:51:16 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,114 @@
+const tl = gsap.timeline({
+    defaults: {
+        ease: "power1.out"
+    }
+});
+
+tl.to(".text", {
+    y: "0%",
+    duration: 1,
+    stagger: 0.25
+});
+tl.to(".preloader__second", {
+    y: "-110%",
+    duration: 1.6,
+    delay: 0.5
+});
+tl.to(".preloader__content", {
+    y: "-180%",
+    duration: 0.7
+}, "-=1");
+window.onload = function () {
+    document.body.classList.add('preloader');
+    window.setTimeout(function () {
+        document.body.classList.add('loaded');
+        document.body.classList.remove('preloader');
+    }, 3050);
+}
+
+const slidePage = document.querySelector(".slide-page");
+const nextBtnFirst = document.querySelector(".firstNext");
+const prevBtnSec = document.querySelector(".prev-1");
+const nextBtnSec = document.querySelector(".next-1");
+const prevBtnThird = document.querySelector(".prev-2");
+const nextBtnThird = document.querySelector(".next-2");
+const prevBtnFourth = document.querySelector(".prev-3");
+const nextBtnFourth = document.querySelector(".next-3");
+const prevBtnFive = document.querySelector(".prev-4");
+const submitBtn = document.querySelector(".submit");
+const progressText = document.querySelectorAll(".step p");
+const progressCheck = document.querySelectorAll(".step .check");
+const bullet = document.querySelectorAll(".step .bullet");
+let current = 1;
+
+nextBtnFirst.addEventListener("click", function (event) {
+    event.preventDefault();
+    slidePage.style.marginLeft = "-25%";
+    bullet[current - 1].classList.add("active");
+    progressCheck[current - 1].classList.add("active");
+    progressText[current - 1].classList.add("active");
+    current += 1;
+});
+nextBtnSec.addEventListener("click", function (event) {
+    event.preventDefault();
+    slidePage.style.marginLeft = "-52%";
+    bullet[current - 1].classList.add("active");
+    progressCheck[current - 1].classList.add("active");
+    progressText[current - 1].classList.add("active");
+    current += 1;
+});
+nextBtnThird.addEventListener("click", function (event) {
+    event.preventDefault();
+    slidePage.style.marginLeft = "-78%";
+    bullet[current - 1].classList.add("active");
+    progressCheck[current - 1].classList.add("active");
+    progressText[current - 1].classList.add("active");
+    current += 1;
+});
+nextBtnFourth.addEventListener("click", function (event) {
+    event.preventDefault();
+    slidePage.style.marginLeft = "-104.4%";
+    bullet[current - 1].classList.add("active");
+    progressCheck[current - 1].classList.add("active");
+    progressText[current - 1].classList.add("active");
+    current += 1;
+});
+submitBtn.addEventListener("click", function () {
+    bullet[current - 1].classList.add("active");
+    progressCheck[current - 1].classList.add("active");
+    progressText[current - 1].classList.add("active");
+    current += 1;
+});
+
+prevBtnSec.addEventListener("click", function (event) {
+    event.preventDefault();
+    slidePage.style.marginLeft = "0%";
+    bullet[current - 2].classList.remove("active");
+    progressCheck[current - 2].classList.remove("active");
+    progressText[current - 2].classList.remove("active");
+    current -= 1;
+});
+prevBtnThird.addEventListener("click", function (event) {
+    event.preventDefault();
+    slidePage.style.marginLeft = "-25%";
+    bullet[current - 2].classList.remove("active");
+    progressCheck[current - 2].classList.remove("active");
+    progressText[current - 2].classList.remove("active");
+    current -= 1;
+});
+prevBtnFourth.addEventListener("click", function (event) {
+    event.preventDefault();
+    slidePage.style.marginLeft = "-52.3%";
+    bullet[current - 2].classList.remove("active");
+    progressCheck[current - 2].classList.remove("active");
+    progressText[current - 2].classList.remove("active");
+    current -= 1;
+});
+prevBtnFive.addEventListener("click", function (event) {
+    event.preventDefault();
+    slidePage.style.marginLeft = "-78.3%";
+    bullet[current - 2].classList.remove("active");
+    progressCheck[current - 2].classList.remove("active");
+    progressText[current - 2].classList.remove("active");
+    current -= 1;
+});
